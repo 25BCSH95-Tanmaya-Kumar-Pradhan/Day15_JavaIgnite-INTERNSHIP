def9994c6e3a6bf402d392d4ad274188d0da05a1
25BCSH95-Tanmaya-Kumar-Pradhan <ultraaeonus@gmail.com>
2026-06-11 15:18:49 +0530

    Update Online_Food_Order_Notification.java

--- Multithreading/Online_Food_Order_Notification.java
+++ Multithreading/Online_Food_Order_Notification.java
@@ -92,3 +92,56 @@ Main Method
     Call placeOrder()
 
 */
+interface NotificationService {
+    void sendMessage();
+}
+
+class EmailNotification implements NotificationService {
+
+    @Override
+    public void sendMessage() {
+        System.out.println("Email Notification Sent");
+    }
+}
+
+class SMSNotification implements NotificationService {
+
+    @Override
+    public void sendMessage() {
+        System.out.println("SMS Notification Sent");
+    }
+}
+
+class OrderService {
+
+    private NotificationService notificationService;
+
+    public OrderService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    public void placeOrder() {
+        System.out.println("Order Confirmed");
+        notificationService.sendMessage();
+    }
+}
+
+public class Online_food_order_notification {
+
+    public static void main(String[] args) {
+    	// TODO Auto-generated method stub
+    	
+        NotificationService email = new EmailNotification();
+        OrderService order1 = new OrderService(email);
+
+        order1.placeOrder();
+
+        System.out.println();
+
+       
+        NotificationService sms = new SMSNotification();
+        OrderService order2 = new OrderService(sms);
+
+        order2.placeOrder();
+    }
+}
